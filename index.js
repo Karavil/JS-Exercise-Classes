8025f5ec4ad4dbf78648d15b349bce9d911b5f01
Alex Karavil <alex@karavil.io>
2020-01-30 13:35:12 -0500

    Done with ProjectManager class, done with project.

--- index.js
+++ index.js
@@ -198,7 +198,21 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {}
+class ProjectManager extends Instructor{
+   constructor(manager) {
+      super(manager);
+      this.gradClassName = manager.gradClassName;
+      this.favInstructor = manager.favInstructor;
+   }
+   
+   standUp(slackChannelName) {
+      return `${this.name} announces to ${slackChannelName}, @channel standy times`;
+   }
+
+   debugsCode(student, subject) {
+      return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
+}
 
 /*
   STRETCH PROBLEM (no tests!)
